8be611c3c902481f872005cf3f404c1204b29b94
Fishynastix <heindrichjansen@gmail.com>
2025-08-30 08:59:47 +0200

    Observer and the notifiers done

--- Observer.cpp
+++ Observer.cpp
@@ -1,16 +1,15 @@
 #include "Observer.h"
 
+
+
 void Customer::update(std::string message) {
-	// TODO - implement Customer::update
-	throw "Not yet implemented";
+    std::cout << "Customer " << name << " received: " << message << std::endl;
 }
 
-Customer::Customer(std::string name, bool isFamily) {
-	// TODO - implement Customer::Customer
-	throw "Not yet implemented";
-}
+Customer::Customer(std::string name, bool isFamily) : name(name), isFamily(isFamily) {}
+
+bool Customer::isFamily() { return isFamily; } // Helper for FamilyDiscount
 
 void Website::update(std::string message) {
-	// TODO - implement Website::update
-	throw "Not yet implemented";
+    std::cout << "Website updated: " << message << std::endl;
 }
--- Observer.h
+++ Observer.h
@@ -2,6 +2,7 @@
 #define OBSERVER_H
 
 #include <string>
+#include <iostream>
 
 class Observer {
 public:
